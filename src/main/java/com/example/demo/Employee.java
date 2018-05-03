7c0da8adbbac267d5414f62ac0fb60d1f3424290
xuexue <1047504737@qq.com>
2018-05-03 13:53:05 +0800

    Employee coding

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+public class Employee {
+    private long id;
+    private String name;
+    private int age;
+    private String gender;
+
+    public Employee(long id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
